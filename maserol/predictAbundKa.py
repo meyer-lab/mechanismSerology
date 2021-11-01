2889681c3a1e8681655773068d0cd0b9ae94b4b9
cztan <cyztan@gmail.com>
2021-11-01 13:22:46 -0700

    Fix predictAbundKa

--- maserol/predictAbundKa.py
+++ maserol/predictAbundKa.py
@@ -1,59 +1,33 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-# r = # of antibody types/bead
 from valentbind import polyfc
-from .import_kaplonek import flattenSpaceX
 
-#first function: generate abundance and Ka matrices from random values
-
-def generate_AbundKa():
-    SX_flatCube, SX_subxant_names, SX_unique_rec_names = flattenSpaceX()
-    R_guess = np.random.randint(4, high=11, size=(1,SX_flatCube[0,:].size), dtype=int)
-    Ka_guess = np.random.randint(1e8,high=9e8, size=(SX_flatCube[:,0].size,1), dtype=int)
-
+def initial_AbundKa(flatCube, n_ab=1):
+    """
+    generate abundance and Ka matrices from random values
+    flatCube.shape == n_rec * (n_subj * n_Ag)
+    """
+    R_guess = np.random.randint(4, high=11, size=(n_ab, flatCube.shape[1]), dtype=int)
+    Ka_guess = np.random.randint(1e8,high=9e8, size=(flatCube.shape[0], n_ab), dtype=int)
     return R_guess, Ka_guess
 
 
-#second function: pass the matrices generated above into polyfc, run through each receptor and antxsub pair and store in matrix same size as flatten
-def infer_Lbound(n_rec, n_samp, n_ab):
-    SX_flatCube, SX_subxant_names, SX_unique_rec_names = flattenSpaceX()
-    R_guess, Ka_guess = generate_AbundKa()
-    assert(R_guess.shape[1] == n_rec)
-    assert(Ka_guess.shape[0] == n_samp)
-
-    #Does experimental data contain nan? -> change to 0
-    if np.isnan(SX_flatCube).any():
-        nan_data = np.isnan(SX_flatCube)
-        nan_row,nan_col = np.where(nan_data == 1)
-        SX_flatCube = np.where(np.isfinite(SX_flatCube), SX_flatCube, 0)
-
-
-    Lbound_guess = np.zeros((SX_flatCube.shape))
-    L0 = np.array([1e-9])
-    KxStar = 1e-12
-    f = 2
+def infer_Lbound(R, Ka, L0=1e-9, KxStar=1e-12):
+    """
+    pass the matrices generated above into polyfc, run through each receptor
+    and ant x sub pair and store in matrix same size as flatten
+    """
+    Lbound_guess = np.zeros((Ka.shape[0], R.shape[1]))
     LigC = np.array([1])
-    for ii in range(Lbound_guess[:,0].size):
-        for xx in range(Lbound_guess[0,:].size):
-            Lbound, _, _ = polyfc(L0, KxStar, f, R_guess[0,xx], LigC, np.array([Ka_guess[ii,0]]))
-            Lbound_guess[ii,xx] = Lbound
-
-    return Lbound_guess, SX_flatCube
+    for ii in range(Lbound_guess.shape[0]):
+        for xx in range(Lbound_guess.shape[1]):
+            Lbound_guess[ii,xx] = polyfc(L0, KxStar, 2, R[:,xx], LigC, Ka[[ii],:])[0]
+    return Lbound_guess
 
 
-#third function loss function comparing model and flattenSpaceX output
-def model_lossfunc():
-    SX_flatCube, SX_subxant_names, SX_unique_rec_names = flattenSpaceX()
-
-    Lbound_guess, SX_flatCube = infer_Lbound(SX_subxant_names.size,SX_unique_rec_names.size,1)
-
-    model_loss = np.zeros((SX_flatCube.shape))
-    for ii in range(Lbound_guess[:,0].size):
-        for xx in range(Lbound_guess[0,:].size):
-            indiv_loss = (Lbound_guess[ii,xx] - SX_flatCube[ii,xx])**2
-            model_loss[ii,xx] = indiv_loss
-
-    model_loss_all = np.sum(model_loss)
-
-    return model_loss, model_loss_all
+def model_lossfunc(R, Ka, flatCube):
+    """
+    loss function comparing model and flattenSpaceX output
+    """
+    Lbound_guess = infer_Lbound(R, Ka)
+    model_loss = np.nansum((Lbound_guess - flatCube)**2)
+    return model_loss
--- maserol/test/test_predictAbundKa.py
+++ maserol/test/test_predictAbundKa.py
@@ -1,6 +1,7 @@
-from ..predictAbundKa import generate_AbundKa, infer_Lbound, model_lossfunc
+from ..predictAbundKa import initial_AbundKa, model_lossfunc
+from ..import_kaplonek import *
 
-def test_run():
-    generate_AbundKa()
-    infer_Lbound()
-    model_lossfunc()
+def test_SpaceX():
+    flatCube, _, _ = flattenSpaceX()
+    R_guess, Ka_guess = initial_AbundKa(flatCube, 3)
+    loss = model_lossfunc(R_guess, Ka_guess, flatCube)
