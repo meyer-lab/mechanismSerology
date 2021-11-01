227ef24cc24472247ece531519935df9bfc4de20
cztan <cyztan@gmail.com>
2021-11-01 13:25:39 -0700

    Tiny formatting

--- maserol/predictAbundKa.py
+++ maserol/predictAbundKa.py
@@ -7,7 +7,7 @@ def initial_AbundKa(flatCube, n_ab=1):
     flatCube.shape == n_rec * (n_subj * n_Ag)
     """
     R_guess = np.random.randint(4, high=11, size=(n_ab, flatCube.shape[1]), dtype=int)
-    Ka_guess = np.random.randint(1e8,high=9e8, size=(flatCube.shape[0], n_ab), dtype=int)
+    Ka_guess = np.random.randint(1e8, high=9e8, size=(flatCube.shape[0], n_ab), dtype=int)
     return R_guess, Ka_guess
 
 
@@ -20,14 +20,12 @@ def infer_Lbound(R, Ka, L0=1e-9, KxStar=1e-12):
     LigC = np.array([1])
     for ii in range(Lbound_guess.shape[0]):
         for xx in range(Lbound_guess.shape[1]):
-            Lbound_guess[ii,xx] = polyfc(L0, KxStar, 2, R[:,xx], LigC, Ka[[ii],:])[0]
+            Lbound_guess[ii,xx] = polyfc(L0, KxStar, 2, R[:, xx], LigC, Ka[[ii], :])[0]
     return Lbound_guess
 
 
 def model_lossfunc(R, Ka, flatCube):
-    """
-    loss function comparing model and flattenSpaceX output
-    """
+    # loss function, comparing model output and flattened tensor
     Lbound_guess = infer_Lbound(R, Ka)
     model_loss = np.nansum((Lbound_guess - flatCube)**2)
     return model_loss
