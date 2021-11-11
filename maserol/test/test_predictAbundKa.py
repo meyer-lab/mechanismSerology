9af9d2139018b5b809e718610856b9f9a1b20c71
erbozich <91349605+erbozich@users.noreply.github.com>
2021-11-11 11:05:47 -0800

    Added optimization/final run of polyfc. Also combined fitting params in one array. (#10)
    * Added optimization/final run of polyfc. Also combined fitting params in one array.
    
    * Some changes in optimization
    
    * Switched to optimize.scipy, other small changes
    
    * Some cleanup
    
    * Implement low-rank abundance assumption
    
    * Make test that will finish
    
    Co-authored-by: Aaron Meyer <ameyer@ucla.edu>
    Co-authored-by: cztan <cyztan@gmail.com>
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- maserol/test/test_predictAbundKa.py
+++ maserol/test/test_predictAbundKa.py
@@ -1,8 +1,11 @@
+import numpy as np
 from ..predictAbundKa import initial_AbundKa, model_lossfunc
-from ..data.kaplonek import flattenSpaceX
+from ..data.kaplonek import cubeSpaceX
 
 
 def test_SpaceX():
-    flatCube, _, _ = flattenSpaceX()
-    R_guess, Ka_guess = initial_AbundKa(flatCube, 3)
-    loss = model_lossfunc(R_guess, Ka_guess, flatCube)
+    cube = cubeSpaceX()
+    R_subj_guess, R_Ag_guess, Ka_guess = initial_AbundKa(cube, 1)
+    x0 = np.concatenate((R_subj_guess.flatten(), R_Ag_guess.flatten(), Ka_guess.flatten()))
+    RKa_opt = model_lossfunc(x0, cube, 1e-9, 1e-12)
+    assert RKa_opt > 0.0
