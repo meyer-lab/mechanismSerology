8c834f9c7611ba658ae392acaefcb044457b1937
Jamie Stickelmaier <69060993+jstickelmaier@users.noreply.github.com>
2021-11-15 13:04:51 -0800

    import and create cube for atyeo2020 data (#9)
    * Import and create cube for atyeo2020 data
    
    * add tests to import atyeo file
    
    * edit test file
    
    * add flatten cube function and test
    
    * try test file again
    
    * fixed test file line breaks
    
    * Fix Atyeo test
    
    * fix load_file
    
    Co-authored-by: cztan <cyztan@gmail.com>

--- maserol/test/test_import.py
+++ maserol/test/test_import.py
@@ -1,21 +1,29 @@
 """
 Unit test file.
 """
-from ..data.alter import importLuminex, createCube
-from ..data.kaplonek import importSpaceX, flattenSpaceX, cubeSpaceX, importMGH, cubeMGH, flattenMGH
-
 
 def test_imports_alter():
     """ Test that files are successfully loaded. """
+    from ..data.alter import importLuminex, createCube
     importLuminex()
     createCube()
 
 
 def test_imports_kaplonek():
     """ Test that files are successfully loaded. """
+    from ..data.kaplonek import importSpaceX, flattenSpaceX, cubeSpaceX, importMGH, cubeMGH, flattenMGH
     importSpaceX()
     cubeSpaceX()
     flattenSpaceX()
     importMGH()
     cubeMGH()
     flattenMGH()
+
+
+def test_import_atyeo():
+    """ Test that files are successfully loaded. """
+    from ..data.atyeo import load_file, getAxes, createCube, flattenCube
+    load_file("atyeo_covid")
+    getAxes()
+    createCube()
+    flattenCube()
