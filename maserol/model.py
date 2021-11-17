a7e130d772675af4ad6f8c074e30af5fcf18d2c9
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2021-11-17 07:26:27 -0800

    Use Jacobian-vector product rather than full Jacobian (#13)
    * A little improvement
    
    * Fix

--- maserol/model.py
+++ maserol/model.py
@@ -3,8 +3,8 @@
 from os.path import join, dirname
 import numpy as np
 import pandas as pd
-from scipy.sparse import csr_matrix
-from jax import jacrev, jit
+from scipy.sparse.linalg import LinearOperator
+from jax import vjp, jit
 from jax.config import config
 import jax.numpy as jnp
 from scipy.optimize import least_squares
@@ -16,10 +16,8 @@ path_here = dirname(dirname(__file__))
 config.update("jax_enable_x64", True)
 
 
-def Req_func(Req: np.ndarray, Rtot: np.ndarray, L0: float, KxStar: float, Kav: np.ndarray):
+def Req_func(Req: np.ndarray, Rtot: np.ndarray, L0fA: np.ndarray, AKxStar: np.ndarray):
     """ Mass balance. """
-    L0fA = L0 * 2 * Kav
-    AKxStar = Kav * KxStar
     Phisum = jnp.dot(AKxStar, Req.T)
     term = jnp.einsum("ij,kil,ilk->kil", L0fA, Req, 1 + Phisum)
     return Req + term - Rtot
@@ -33,20 +31,24 @@ def lBnd(L0: float, KxStar, Rtot, Kav):
     Rtot: numbers of each receptor appearing on the cell.
     Kav: a matrix of Ka values. row = ligands, col = receptors
     """
-    # Run least squares to get Req
-    def bal(x):
-        xR = np.reshape(x, Rtot.shape)
-        return Req_func(xR, Rtot, L0, KxStar, Kav).flatten()
-
-    jacc = jit(jacrev(bal))
-
-    def jaccFunc(x):
-        J = csr_matrix(jacc(x))
-        J.eliminate_zeros()
-        return J
+    L0fA = L0 * 2 * Kav
+    AKxStar = Kav * KxStar
 
     x0 = Rtot.flatten()
     bnd = (0.0, Rtot.flatten())
+
+    # Run least squares to get Req
+    def bal(x):
+        xR = jnp.reshape(x, Rtot.shape)
+        return Req_func(xR, Rtot, L0fA, AKxStar).flatten()
+
+    # Define a Jacobian linear operator so we don't have to enumerate it
+    def jaccFunc(x):
+        _, f_vjp = vjp(bal, x)
+        f_vjp = jit(f_vjp)
+        funcc = lambda x: f_vjp(np.squeeze(x))
+        return LinearOperator((x0.size, x0.size), rmatvec=funcc, matvec=funcc)
+    
     x0 = x0 / (1.0 + 2.0 * L0 * np.amax(Kav)) # Monovalent guess using highest affinity
     lsq = least_squares(bal, x0, jac=jaccFunc, bounds=bnd, xtol=1e-9, tr_solver="lsmr")
     assert lsq.success, "Failure in rootfinding. " + str(lsq)
