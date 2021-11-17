279177a0bb3bbc0ca11866fced689d951bc1d56a
Aaron Meyer <ameyer@ucla.edu>
2021-11-17 07:13:12 -0800

    Fix

--- maserol/predictAbundKa.py
+++ maserol/predictAbundKa.py
@@ -5,62 +5,9 @@ Polyfc output is compared to SpaceX data and cost function is minimzied through 
 Total fitting parameters = 147
 """
 import numpy as np
-from scipy.sparse.linalg import LinearOperator
-from jax import vjp, jit
-from jax.config import config
-import jax.numpy as jnp
-from scipy.optimize import minimize, least_squares
+from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from .model import lBnd
-
-
-config.update("jax_enable_x64", True)
-
-
-def Req_func(Req: np.ndarray, Rtot: np.ndarray, L0fA: np.ndarray, AKxStar: np.ndarray):
-    """ Mass balance. """
-    Phisum = jnp.dot(AKxStar, Req.T)
-    term = jnp.einsum("ij,kil,ilk->kil", L0fA, Req, 1 + Phisum)
-    return Req + term - Rtot
-
-
-def lBnd(L0: float, KxStar, Rtot, Kav):
-    """
-    The main function. Generate all info for heterogenenous binding case
-    L0: concentration of ligand complexes.
-    KxStar: detailed balance-corrected Kx.
-    Rtot: numbers of each receptor appearing on the cell.
-    Kav: a matrix of Ka values. row = ligands, col = receptors
-    """
-    L0fA = L0 * 2 * Kav
-    AKxStar = Kav * KxStar
-
-    x0 = Rtot.flatten()
-    bnd = (0.0, Rtot.flatten())
-
-    # Run least squares to get Req
-    def bal(x):
-        xR = jnp.reshape(x, Rtot.shape)
-        return Req_func(xR, Rtot, L0fA, AKxStar).flatten()
-
-    # Define a Jacobian linear operator so we don't have to enumerate it
-    def jaccFunc(x):
-        _, f_vjp = vjp(bal, x)
-        f_vjp = jit(f_vjp)
-        funcc = lambda x: f_vjp(np.squeeze(x))
-        return LinearOperator((x0.size, x0.size), rmatvec=funcc, matvec=funcc)
-    
-    x0 = x0 / (1.0 + 2.0 * L0 * np.amax(Kav)) # Monovalent guess using highest affinity
-    lsq = least_squares(bal, x0, jac=jaccFunc, bounds=bnd, xtol=1e-9, tr_solver="lsmr")
-    assert lsq.success, "Failure in rootfinding. " + str(lsq)
-
-    Req = np.reshape(lsq.x, Rtot.shape)
-
-    AKxStar = Kav * KxStar
-    Phisum = np.dot(AKxStar, Req.T)
-
-    Lbound = L0 / KxStar * ((1 + Phisum) ** 2 - 1)
-    return np.squeeze(Lbound).T
 
 
 def initial_AbundKa(cube, n_ab=1):
