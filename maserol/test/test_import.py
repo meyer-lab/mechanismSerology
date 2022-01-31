66882752d691a25233ee7ab04e4a2b7ae6a5062e
Aaron Meyer <ameyer@ucla.edu>
2022-01-31 08:08:04 -0800

    Last cleanup

--- maserol/test/test_import.py
+++ maserol/test/test_import.py
@@ -9,17 +9,6 @@ def test_imports_alter():
     createCube()
 
 
-def test_imports_kaplonek():
-    """ Test that files are successfully loaded. """
-    from ..data.kaplonek import importSpaceX, flattenSpaceX, cubeSpaceX, importMGH, cubeMGH, flattenMGH
-    importSpaceX()
-    cubeSpaceX()
-    flattenSpaceX()
-    importMGH()
-    cubeMGH()
-    flattenMGH()
-
-
 def test_import_atyeo():
     """ Test that files are successfully loaded. """
     from ..data.atyeo import load_file, getAxes, createCube, flattenCube
--- maserol/test/test_predictAbundKa.py
+++ maserol/test/test_predictAbundKa.py
@@ -1,12 +1,12 @@
 import pytest
 import numpy as np
 from ..predictAbundKa import initial_AbundKa, model_lossfunc, optimize_lossfunc
-from ..data.kaplonek import cubeSpaceX
+from ..data.atyeo import createCube
 
 
 @pytest.mark.parametrize("n_ab", [1, 2, 3])
-def test_SpaceX(n_ab):
-    cube = cubeSpaceX()
+def test_fit(n_ab):
+    cube = createCube()
     R_subj_guess, R_Ag_guess, Ka_guess = initial_AbundKa(cube, n_ab)
     x0 = np.concatenate((R_subj_guess.flatten(), R_Ag_guess.flatten(), Ka_guess.flatten()))
     RKa_opt = model_lossfunc(x0, cube, 1e-9, 1e-12)
