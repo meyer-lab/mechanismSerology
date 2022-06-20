42848696949509934c0f9153a5bee2c195e1e7f5
Cyrillus Tan <cyztan@gmail.com>
2022-06-20 13:23:44 -0700

    Optimize colormap (#24)
    * make heatmap figure for atyeo compnent weights
    
    * working on optimizing colormap
    
    * Make components plot
    
    * Remove common.
    
    * Black formatting.
    
    * Fix some small bugs in plotting
    
    * Remove common
    
    Co-authored-by: jstickelmaier <jamie.stickelmaier@gmail.com>
    Co-authored-by: Aaron Meyer <ameyer@ucla.edu>

--- maserol/figures/figure3.py
+++ maserol/figures/figure3.py
@@ -1,37 +1,10 @@
-import numpy as np
-from matplotlib import gridspec, pyplot as plt
-import pandas as pd
-from tensordata.atyeo import createCube, getAxes
-import seaborn as sns
+from tensordata.atyeo import data as atyeo
 from tensorpack import perform_CP
+from ..plots import makeComponentPlot
+
 
 def makeFigure():
-    """ Generate heatmap plots for each input dimension by component"""
-    cube = createCube()
-    subjects, receptors, antigens = getAxes()
-    tfac = perform_CP(tOrig=cube)
-
-    components =  [str(ii + 1) for ii in range(tfac.rank)]
-
-    subs = pd.DataFrame(tfac.factors[0], columns=[f"Cmp. {i}" for i in np.arange(1, tfac.rank + 1)], index=subjects)
-    rec = pd.DataFrame(tfac.factors[1], columns=[f"Cmp. {i}" for i in np.arange(1, tfac.rank + 1)], index=receptors)
-    ant = pd.DataFrame(tfac.factors[2], columns=[f"Cmp. {i}" for i in np.arange(1, tfac.rank + 1)], index=antigens)
-    
-    f = plt.figure(figsize=(16, 6))
-    gs = gridspec.GridSpec(1, 3, wspace=0.5)
-    ax1 = plt.subplot(gs[0])
-    ax2 = plt.subplot(gs[1])
-    ax3 = plt.subplot(gs[2])
-
-    sns.heatmap(subs, cmap="PiYG", center=0, xticklabels=components, yticklabels=subjects, cbar=True, vmin=-1.0, vmax=1.0, ax=ax1)
-    sns.heatmap(rec, cmap="PiYG", center=0, xticklabels=components, yticklabels=receptors, cbar=False, vmin=-1.0, vmax=1.0, ax=ax2)
-    sns.heatmap(ant, cmap="PiYG", center=0, xticklabels=components, yticklabels=antigens, cbar=False, vmin=-1.0, vmax=1.0, ax=ax3)
-
-    ax1.set_xlabel("Components")
-    ax1.set_title("Subjects")
-    ax2.set_xlabel("Components")
-    ax2.set_title("Receptors")
-    ax3.set_xlabel("Components")
-    ax3.set_title("Antigens")
-
-    return f
+    """Generate heatmap plots for each input dimension by component"""
+    data = atyeo()
+    tfac = perform_CP(tOrig=data.tensor)
+    return makeComponentPlot([tfac.factors[0], tfac.factors[1], tfac.factors[2]], data.axes)
---
+++ maserol/figures/figure4.py
@@ -0,0 +1,11 @@
+from tensordata.atyeo import data as atyeo
+from ..predictAbundKa import *
+from ..plots import makeComponentPlot
+
+
+def makeFigure():
+    """ """
+    data = atyeo()
+    opt = optimize_lossfunc(data.tensor, n_ab=2)
+    R_subj, R_Ag, Ka = reshapeParams(opt, data.tensor)
+    return makeComponentPlot([R_subj, Ka, R_Ag], data.axes)
