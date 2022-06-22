7f0a71e0d603365287cf32f5afa27a4945f5871f
Cyrillus <cyztan@gmail.com>
2022-06-22 12:11:58 -0700

    Add normalization to concatenated data

--- maserol/linear.py
+++ maserol/linear.py
@@ -2,9 +2,23 @@ from tensordata.kaplonek import MGH, MGH4D, SpaceX
 from tensordata.zohar import data3D
 import numpy as np
 
+def checkMissingess(cube):
+    return 1 - np.sum(np.isfinite(cube))/np.product(cube.shape)
+
+
+def normalizeSubj(cube):
+    cube -= np.nanmean(cube, axis=0)
+    cube = cube / np.nanstd(cube, axis=0)
+    return cube
+
+
 """ Assemble the concatenated COVID tensor in 3D """
 def importConcat():
     M, S, Z = MGH(xarray=True), SpaceX(xarray=True), data3D(xarray=True)
+
+    M = normalizeSubj(M)
+    S = normalizeSubj(S)
+    Z = normalizeSubj(Z)
 
     # Set consistent antigen naming
     M_dict, S_dict, Z_dict = {'Antigen': ['SARS.CoV2_N', 'CoV.OC43', 'Flu_HA', 'SARS.CoV2_S1', 'Ebola_gp', 'CMV',
@@ -17,11 +31,10 @@ def importConcat():
                                              'SARS.CoV2_S2', 'SARS.CoV2_S1trimer']}
 
     M, S, Z = M.assign_coords(M_dict), S.assign_coords(S_dict), Z.assign_coords(Z_dict)
-    combined = S.combine_first(M).combine_first(Z)
+    cube = S.combine_first(M).combine_first(Z)
+    cube = normalizeSubj(cube)
 
-    ## TODO: normalize data correctly
-
-    return combined
+    return cube
 
 def importConcat4D():
     pass
