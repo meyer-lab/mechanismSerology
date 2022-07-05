c5e9015df9a1971e10f06ae3b72f7b68eefc99de
Cyrillus Tan <cyztan@gmail.com>
2022-07-05 03:14:20 -0700

    Set up initial value matrix for affinity (#29)
    * Move an old plotting code to another file
    
    * Start some skeleton code for assembling Kav matrix
    
    * changed dimension order to subject, antigen, receptor in all funcs
    
    * added code to assemble Kav
    
    * updated Kav matrix generation code with test cases
    
    * Created new file with optimization code when Kav is fixed
    
    * Made minor changes to Kav assembly code
    
    * Fixed bug in prepare_data
    
    * Remove redundant code from optimization
    
    Co-authored-by: priyanka <shrestp@stanford.edu>

--- maserol/figures/figure2.py
+++ maserol/figures/figure2.py
@@ -1,6 +1,7 @@
 from .common import getSetup, subplotLabel
-from ..predictAbundKa import optimize_lossfunc, plot_correlation_heatmap
+from ..predictAbundKa import *
 from tensordata.atyeo import data as atyeo
+from scipy.stats import pearsonr
 
 def makeFigure():
     axs, f = getSetup((8, 4), (1, 2))
@@ -17,3 +18,31 @@ def makeFigure():
     return f
 
 
+def plot_correlation_heatmap(ax, RKa_opt, cube, rec_names, ant_names):
+    """
+    Uses optimal parameters from optimize_lossfunc to run the model
+    Generates prelim figures to compare experimental and model results
+    R_subj, R_Ag, Ka, L0=L0, KxStar=KxStar
+    """
+
+    R_subj, R_Ag, Ka = reshapeParams(RKa_opt, cube)
+    Lbound_model = infer_Lbound(R_subj, R_Ag, Ka, L0=1e-9, KxStar=1e-12)
+
+    coeff = np.zeros([cube.shape[1], cube.shape[2]])
+    for ii in range(cube.shape[1]):
+        for jj in range(cube.shape[2]):
+            coeff[ii, jj], _ = pearsonr(cube[:, ii, jj], Lbound_model[:, ii, jj])
+
+    print(coeff)
+    ax.imshow(coeff)
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(ant_names)))
+    ax.set_xticklabels(ant_names, rotation=45)
+    ax.set_yticks(np.arange(len(rec_names)))
+    ax.set_yticklabels(rec_names)
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(ant_names)):
+        for j in range(len(rec_names)):
+            text = ax.text(i, j, round(coeff[j, i], 2), ha="center", va="center", color="w")
---
+++ maserol/fixKav_optimization.py
@@ -0,0 +1,67 @@
+import numpy as np
+import jax.numpy as jnp
+from tqdm import tqdm
+from scipy.optimize import minimize
+from jax import value_and_grad, jit, jacfwd, jacrev
+from .predictAbundKa import infer_Lbound
+
+
+def initial_subj_abund(cube, n_ab=1):
+    """
+    Generate subjects and Ka matrices by initializing all values to 10**6
+    cube.shape == n_subj * n_rec * antigen
+    """
+    # TODO: Add masking
+    subj_matrix = np.full((cube.shape[0], n_ab), 10**6)
+    ag_matrix = np.full((cube.shape[2], n_ab), 10**6)
+    return subj_matrix, ag_matrix
+
+def flatten_params(r_subj, r_ag):
+    """ Flatten into a parameter vector"""
+    return np.log(np.concatenate((r_subj.flatten(), r_ag.flatten())))
+
+def reshapeParams(x, cube):
+    """ Unflatten parameter vector back into subject and antigen matrices """
+    x = jnp.exp(x)
+    n_subj, n_rec,  n_Ag = cube.shape
+    n_ab = int(len(x) / (n_subj + n_Ag))
+    R_subj = x[0:(n_subj * n_ab)].reshape(n_subj, n_ab)
+    R_Ag = x[(n_subj * n_ab):((n_subj + n_Ag) * n_ab)].reshape(n_Ag, n_ab)
+    return R_subj, R_Ag
+
+def model_lossfunc(x, cube, Ka, L0=1e-9, KxStar=1e-12):
+    """
+    Loss function, comparing model output and flattened tensor
+    """
+    R_subj, R_Ag = reshapeParams(x, cube)
+    Lbound = infer_Lbound(R_subj, R_Ag, Ka, L0=L0, KxStar=KxStar)
+    diff = jnp.log(cube) - jnp.log(Lbound)
+    diff -= jnp.nanmean(diff)
+    return jnp.linalg.norm(jnp.nan_to_num(diff))
+
+def optimize_lossfunc(cube, Kav, n_ab=1, maxiter=100):
+    """
+    Optimization method to minimize model_lossfunc output
+    """
+    R_subj_guess, R_Ag_guess = initial_subj_abund(cube, n_ab=n_ab)
+    Kav[np.where(Kav==0.0)] = 10
+    Kav_log = np.log(Kav)
+    x0 = flatten_params(R_subj_guess, R_Ag_guess)
+
+    func = jit(value_and_grad(model_lossfunc))
+    hess = jit(jacfwd(jacrev(model_lossfunc)))
+    opts = {'maxiter': maxiter}
+    arrgs = (cube, Kav_log, 1e-9, 1e-12)
+
+    with tqdm(total=maxiter, delay=0.1) as tq:
+        def callback(xk):
+            a, b = func(xk, *arrgs)
+            gNorm = np.linalg.norm(b)
+            tq.set_postfix(val='{:.2e}'.format(a), g='{:.2e}'.format(gNorm), refresh=False)
+            tq.update(1)
+
+        print("")
+
+        opt = minimize(func, x0, method="trust-ncg", args=arrgs, hess=hess, callback=callback, jac=True, options=opts)
+    return opt.x[:, np.newaxis]
+
