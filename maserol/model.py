3e4eb64b47515330f31dcf0e59db048437f312b8
priyanka <shrestp@stanford.edu>
2022-07-06 22:17:42 -0700

    Cleaned up functions in model.py

--- maserol/model.py
+++ maserol/model.py
@@ -12,29 +12,19 @@ initial_affinity = 10**8
 mode_order = ["Sample", "Receptor", "Antigen"]
 
 def human_affinity():
-    # read in as a DataFrame
+    """
+    Return a dataFrame of known affinity measurments
+    """
     df = pd.read_csv(join(path_here, "maserol/data/human-affinities.csv"),
                        delimiter=",", comment="#", index_col=0)
     df.drop(["FcgRIIA-131R", "FcgRIIB-232T", "FcgRIIIA-158F"], inplace=True)
     return df
 
-def standardize_dim_order(data, order_list):
-    ' '' Transposes data to be in ("Sample", "Antigen", "Receptor") order '''
-    return data.transpose(order_list[0], order_list[1], order_list[2])
-
-def omit_unnecessary_receptors(data, abs="IgG"):
-    ''' Omits all receptor data from the initial data that does not pertain to the specified antibody'''
-    data_receptors = data.Receptor
-
-    # edit to account for receptors other than IgG
-    wanted_receptors = [x for x in data_receptors.values if x.startswith(abs)] + \
-                       [x for x in data_receptors.values if (x.startswith("FcR") and x != "FcRalpha")]
-
-    return data.sel(Receptor=wanted_receptors)
-
 def get_affinity(affinities_df, receptor, abs):
-    ''' Given a receptor and antibody pair and dataFrame of known affinity values,
-        returns the associatied human affinity value'''
+    """ 
+    Given a receptor and antibody pair and dataFrame of known affinity values,
+    returns the associatied human affinity value 
+    """
     # figure out of receptor uses iii or 1,2,3 system
     x = re.search("3|2|1|i+", receptor, flags=re.IGNORECASE)
     match = x.group()
@@ -53,12 +43,24 @@ def get_affinity(affinities_df, receptor, abs):
             return affinities_df.at[r,abs]
     return 0
 
-def prepare_data(data: xr.DataArray):
-    data = standardize_dim_order(data, mode_order)
-    data = omit_unnecessary_receptors(data)
-    return data
+def prepare_data(data: xr.DataArray, order_list, abs="IgG"):
+    """
+    Transposes data to be in ("Sample", "Antigen", "Receptor") order 
+    and omits all receptor data that does not pertain to the specified antibody
+    """
+    data = data.transpose(order_list[0], order_list[1], order_list[2])
+
+    data_receptors = data.Receptor.values
+
+    wanted_receptors = [x for x in data_receptors if x.startswith(abs)] + \
+                       [x for x in data_receptors if (x.startswith("FcR") and x != "FcRalpha")]
+
+    return data.sel(Receptor=wanted_receptors)
 
 def assemble_Kav(data: xr.DataArray):
+    """
+    Assemblies fixed affinities matrix for a given dataset
+    """
     abs = ["IgG1", "IgG2", "IgG3", "IgG4"]
     
     # omit entries irrelevant to IgG and FcRg
