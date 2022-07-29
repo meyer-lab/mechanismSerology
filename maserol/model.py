5868aca4cc93c04dec4d007b7c7306ca908e65ca
Cyrillus Tan <cyztan@gmail.com>
2022-07-29 14:57:19 -0700

    Initial ka (#33)
    * Move an old plotting code to another file
    
    * Start some skeleton code for assembling Kav matrix
    
    * changed dimension order to subject, antigen, receptor in all funcs
    
    * added code to assemble Kav
    
    * updated Kav matrix generation code with test cases
    
    * Created new file with optimization code when Kav is fixed
    
    * Made minor changes to Kav assembly code
    
    * Fixed bug in prepare_data
    
    * Fixed bugs in fixed Kav optimization
    
    * Changed cube datatype from DataArray to array
    
    * Changed Kav value initialization and added masking to deal with empty values
    
    * Cleaned up functions in model.py
    
    * added tests
    
    * Consolidated code and fixed bugs
    
    * Remove unused imports
    
    Co-authored-by: priyanka <shrestp@stanford.edu>

--- maserol/model.py
+++ maserol/model.py
@@ -1,6 +1,5 @@
 """ Import binding affinities. """
 
-from ast import Pass
 from os.path import join, dirname
 import numpy as np
 import pandas as pd
@@ -12,29 +11,19 @@ initial_affinity = 10**8
 mode_order = ["Sample", "Receptor", "Antigen"]
 
 def human_affinity():
-    # read in as a DataFrame
+    """
+    Return a dataFrame of known affinity measurments
+    """
     df = pd.read_csv(join(path_here, "maserol/data/human-affinities.csv"),
                        delimiter=",", comment="#", index_col=0)
     df.drop(["FcgRIIA-131R", "FcgRIIB-232T", "FcgRIIIA-158F"], inplace=True)
     return df
 
-def standardize_dim_order(data, order_list):
-    ' '' Transposes data to be in ("Sample", "Antigen", "Receptor") order '''
-    return data.transpose(order_list[0], order_list[1], order_list[2])
-
-def omit_unnecessary_receptors(data, abs="IgG"):
-    ''' Omits all receptor data from the initial data that does not pertain to the specified antibody'''
-    data_receptors = data.Receptor
-
-    # edit to account for receptors other than IgG
-    wanted_receptors = [x for x in data_receptors.values if x.startswith(abs)] + \
-                       [x for x in data_receptors.values if (x.startswith("FcR") and x != "FcRalpha")]
-
-    return data.sel(Receptor=wanted_receptors)
-
 def get_affinity(affinities_df, receptor, abs):
-    ''' Given a receptor and antibody pair and dataFrame of known affinity values,
-        returns the associatied human affinity value'''
+    """ 
+    Given a receptor and antibody pair and dataFrame of known affinity values,
+    returns the associatied human affinity value 
+    """
     # figure out of receptor uses iii or 1,2,3 system
     x = re.search("3|2|1|i+", receptor, flags=re.IGNORECASE)
     match = x.group()
@@ -53,23 +42,32 @@ def get_affinity(affinities_df, receptor, abs):
             return affinities_df.at[r,abs]
     return 0
 
-def prepare_data(data: xr.DataArray):
-    data = standardize_dim_order(data, mode_order)
-    data = omit_unnecessary_receptors(data)
-    return data
+def prepare_data(data: xr.DataArray, abs="IgG"):
+    """
+    Transposes data to be in ("Sample", "Antigen", "Receptor") order 
+    and omits all receptor data that does not pertain to the specified antibody
+    """
+    data = data.transpose(mode_order[0], mode_order[1], mode_order[2])
+
+    data_receptors = data.Receptor.values
+
+    wanted_receptors = [x for x in data_receptors if x.startswith(abs)] + \
+                       [x for x in data_receptors if (x.startswith("FcR") and x != "FcRalpha")]
+
+    return data.sel(Receptor=wanted_receptors)
 
 def assemble_Kav(data: xr.DataArray):
+    """
+    Assemblies fixed affinities matrix for a given dataset
+    """
     abs = ["IgG1", "IgG2", "IgG3", "IgG4"]
-    
-    # omit entries irrelevant to IgG and FcRg
-    data = prepare_data(data)
     receptors = data.Receptor.values
 
     # get known affinities
     affinities = human_affinity()
 
     # assemble matrix
-    data_placeholder = np.zeros((len(receptors), len(abs)))
+    data_placeholder = np.full((len(receptors), len(abs)), 10)
     Kav = xr.DataArray(data_placeholder, coords=[receptors, abs], dims=["Receptor", "Abs"])
     
     # separate into list of fc and igg receptors
