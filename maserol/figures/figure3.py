ec7224e7d785c803ede1167222204457f008f8b1
Cyrillus <cyztan@gmail.com>
2022-10-05 18:32:26 -0700

    Remove figure files

--- maserol/figures/figure3.py
+++
@@ -1,8 +0,0 @@
-"""
-Figure 3. MTD accurately predicts unseen FcR interactions
-a) before optimization
-b) after optimization
-c-e) triple plot for results
-f) a schematic for leave-one-receptor-out
-g-i) predicted left out receptor measurement (R2a, R3a, R3b)
-"""
